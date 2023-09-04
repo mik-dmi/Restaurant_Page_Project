881481c6dd15603bbcec3ea9af50aa1de12ee53f
mik-dmi <122227240+mik-dmi@users.noreply.github.com>
2023-09-04 19:17:41 +0100

    Still working on on the home page

--- src/home_page.js
+++ src/home_page.js
@@ -1,11 +1,37 @@
+import DOMPurify from 'dompurify';
 export const mainPage = ()=>{
+    const homePageContainers = [];
+    const titleIfBody = [];
+    const textInBody = [];
+    const dataOfTitleInTheBody = ["Welcome","Oppen Hours","Location"]
+    const dataTextInTheBody = ['Discover culinary excellence at Snacks on the Plate.Our restaurant offers a delightful array of globally inspired snacks and dishes. Experience the perfect blend of comfort and flavor with us today.',
+    `<strong>Monday</strong> to Friday: 11:00 AM - 10:00 PM<br>
+    <strong>Saturday</strong>: 12:00 PM - 11:00 PM<br>
+    <strong>Sunday</strong>: 12:00 PM - 9:00 PM
+    `, `Snacks on the Plate is conveniently located at:<br>
+    123 Main Street,<br>
+    City, State, ZIP Code<br><br>
+    We are situated in the heart of [City], making it easy for you to find us and enjoy our delicious cuisine. We look forward to welcoming you to our restaurant!
+    ` ];
+
+
+
+
 
     const mainContainer = document.getElementById('content');
+
     const headerContainer = document.createElement('div');
+    headerContainer.classList = 'header';
+
     const bodyContainer = document.createElement('div');
+    bodyContainer.classList = 'body';
+
     const footerContainer = document.createElement('div');
-    const homePageContainers = [];
-   
+    footerContainer.classList = 'footer'
+    
+    const sidebarContainer = document.createElement('div');
+    sidebarContainer.classList = "sidebar" 
+
     const homeButton = document.createElement('button');
     homeButton.classList = "menuButton";
     
@@ -19,24 +45,40 @@ export const mainPage = ()=>{
     contactButton.classList = "menuButton";
     
     const nameOfRestaurant = document.createElement('div');
-    nameOfRestaurant.classList = "menuButton";
+    
 
+    
     mainContainer.appendChild(headerContainer); //main page template 
+    mainContainer.appendChild(sidebarContainer); 
     mainContainer.appendChild(bodyContainer);   //main page template 
     mainContainer.appendChild(footerContainer); //main page template 
-
-    headerContainer.appendChild(homeButton);
-    headerContainer.appendChild(menuButton);
-    headerContainer.appendChild(galleryButton);
-    headerContainer.appendChild(contactButton);
-    bodyContainer.appendChild(nameOfRestaurant);
+    
+    headerContainer.appendChild(nameOfRestaurant);
+    sidebarContainer.appendChild(homeButton);
+    sidebarContainer.appendChild(menuButton);
+    sidebarContainer.appendChild(galleryButton);
+    sidebarContainer.appendChild(contactButton);
+   
 
     for(let i = 0; i < 3; i++ ){
         homePageContainers[i] = document.createElement('div');
+        titleIfBody[i] = document.createElement('p');
+        titleIfBody[i].classList = 'titleInBody';
+        textInBody[i] = document.createElement('p');
+        textInBody.classList = 'textInBody';
         homePageContainers[i].classList = "containersHomePage";
-        homePageContainers[i].textContent = "wsadcv"
-        bodyContainer.appendChild(homePageContainers[i])
+        titleIfBody[i].textContent = dataOfTitleInTheBody[i];
+        textInBody[i].innerHTML  = dataTextInTheBody[i];
+        bodyContainer.appendChild(homePageContainers[i]);
+        homePageContainers[i].appendChild(titleIfBody[i]);
+        homePageContainers[i].appendChild(textInBody[i]);
     };
+
+        
+  
+
+
+
      
     nameOfRestaurant.textContent = "Snacks on the Plate"
 
@@ -45,6 +87,7 @@ export const mainPage = ()=>{
     galleryButton.textContent = 'Gallery';
     contactButton.textContent = 'Contact';
 
+    footerContainer.textContent = "Copy Rights"
     console.log("coococ")
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,33 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+module: {
+  rules: [
+      {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          use: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]', // Use placeholders for the output filename
+                outputPath: '../'
+              }
+          }
+      }
+
+    ]
+},
+module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader', // Add Babel loader for ES6 support
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    },
+  ],
+}
 };
